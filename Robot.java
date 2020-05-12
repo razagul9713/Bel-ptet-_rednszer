8b354c79b61ea71c709d9d7f57b39e9aaf140296
razagul9713 <44830556+razagul9713@users.noreply.github.com>
2020-05-12 15:40:12 +0200

    Javítva
    Most már nem dob hibát, azonban a kód így sem működik tökéletesen

--- Robot.java
+++ Robot.java
@@ -7,20 +7,18 @@ package robot1;
 public class Robot1{
 
     
-    public static void main(String[] args)  {
-                Robot r1 = new GyáriRobot();
+   public static void main(String[] args)  {
+                Robot r1 = new GyáriRobot("ID:123",5096,"Férfi",120,"Elv1, Elv2, Elv3");
 		System.out.println(r1);
-		Robot r2 = new SzereloRobot();
+		Robot r2 = new SzereloRobot("ID:456",5096,"Férfi",220,"Elv1, Elv2, Elv3");
 		System.out.println(r2);
-		Robot r3 = new RendörRobot();
+		Robot r3 = new RendörRobot("ID:789",5096,"Férfi",320,"Elv1, Elv2, Elv3");
 		System.out.println(r3);
-		Robot r4 = new HáziRobot();
+		Robot r4 = new HáziRobot("ID:112",5096,"Nő",60,"Elv1, Elv2, Elv3");
 		System.out.println(r4);
-		Robot r5 = new IrodaiRobot();
+		Robot r5 = new IrodaiRobot("ID:113",5096,"Nő",50,"Elv1, Elv2, Elv3");
 		System.out.println(r5);
-	        IWorkable workable = new Robot();
-	        ISleepable sleepable = new Robot();
-	        IWorker worker = new Robot();
+	       
     }
     
 }
@@ -31,25 +29,25 @@ interface Ember{
 	int getIQ();
 	
 }
-interface ISzoftver{
-	String[] getstoftvers = new String[]{KUKA, Windows, Linux};
+interface ISzoftvers{
+	String[] getstoftvers = new String[]{"KUKA", "Windows", "Linux"};
 }
 interface IWorkable{
-	void work();
+	void getwork();
 }
 interface ISleepable{
-	void sleep();
+	void getsleep();
 }
-interface IWorker implements IWorkable, ISleepable{
+interface IWorker extends IWorkable, ISleepable{
 	
 }
 
 
- abstract class Robot implements ISzoftver, IWorker {
+ abstract class Robot implements ISzoftvers, IWorker {
 	String ID;
 	int memoria;
 	String nem;
-	String Szoftver;
+	
 	int teljesitmeny;
 	String elv;
 	public Robot(String ID, int memoria, String nem, int teljesitmeny, String elv){
@@ -58,6 +56,7 @@ interface IWorker implements IWorkable, ISleepable{
 		this.nem = nem;
 		this.teljesitmeny=teljesitmeny;
 		this.elv = elv;
+                
 		
 	}
 	public String getID(){return ID;}
@@ -83,14 +82,14 @@ interface IWorker implements IWorkable, ISleepable{
 		else return "Nem megfelelőek az elvek";
 			
 	}
-	 public void work();
-	 public void sleep();
-	 public String[] getSzoftvers(){
-		 return Szoftver;
+        String Szoftver;
+	
+	 public String[] getSzoftvers(String Szoftver){
+		 return null;
 	 }
 	 
     String kiir() {
-	    return Robot;
+	    return kiir();
     }
 }
 
@@ -115,7 +114,7 @@ class Robot_to_Ember implements Ember{
     	}
 }
 
-abstract class Textúra extends Robot implements ISzoftver, IWorker{
+abstract class Textúra extends Robot implements ISzoftvers, IWorker{
 
 	String Szoftver;
     public Textúra(String ID, int memoria, String nem, int teljesitmeny, String elv) {
@@ -128,17 +127,21 @@ abstract class Textúra extends Robot implements ISzoftver, IWorker{
 	public abstract int getTeljesitmeny();
 	public abstract String getElv();
         public abstract Robot gyariRobot();
-	public void work();
-	public void sleep();
-	 public String[] getSzoftvers(){
-		 return Szoftver;
+	public String[] getSzoftvers(String Szoftver){
+		 return getSzoftvers();
 	 }	
+
+    private String[] getSzoftvers() {
+        return null;
+    }
 }
-
-
- class GyáriRobot extends Textúra implements ISzoftver, IWorker{
+ class GyáriRobot extends Textúra implements ISzoftvers, IWorker{
     Robot gyariRobot;
     String Szoftver;
+
+    public GyáriRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
     public String kiir(Robot gyariRobot){return "GyáriRobot";}
     @Override
     public String getID() {
@@ -165,17 +168,48 @@ abstract class Textúra extends Robot implements ISzoftver, IWorker{
         return "Elv:" + gyariRobot.getElv();
     }
 	 
-    public void work();
-    public void sleep();
-    public String[] getSzoftvers(){
-	 return Szoftver;
+   
+    public String[] getSzoftvers(String Szoftver){
+	 return getSzoftvers();
     }  
+
+    @Override
+    public String kiir() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Robot gyariRobot() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getwork() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getsleep() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
 
+ 
 
-class RendörRobot extends Textúra implements ISzoftver, IWorker{
+   
+
+
+class RendörRobot extends Textúra implements ISzoftvers, IWorker{
     Robot rendorRobot;
     String Szoftver;
+
+    public RendörRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
     @Override
     public String getID() {
        return "RendörTesztRobot ID" + rendorRobot.getID();
@@ -200,16 +234,42 @@ class RendörRobot extends Textúra implements ISzoftver, IWorker{
     public String getElv() {
         return "RendörTesztRobot Elve: " + rendorRobot.getElv();
     }
-    public void work();
-    public void sleep();
-    public String[] getSzoftvers(){
-        return Szoftver;
+    public String[] getSzoftvers(String Szoftver){
+        return getSzoftvers();
+    }
+
+    @Override
+    public String kiir() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getwork() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getsleep() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Robot gyariRobot() {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 }
 
-class HáziRobot extends Textúra implements ISzoftver, IWorker{
+class HáziRobot extends Textúra implements ISzoftvers, IWorker{
     Robot haziRobot;
     String Szoftver;
+
+    public HáziRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
     @Override
     public String getID() {
        return "RendörTesztRobot ID" + haziRobot.getID();
@@ -234,17 +294,43 @@ class HáziRobot extends Textúra implements ISzoftver, IWorker{
     public String getElv() {
         return "RendörTesztRobot Elve: " + haziRobot.getElv();
     }
-    public void work();
-    public void sleep();
-    public String[] getSzoftvers(){
-        return Szoftver;
+    public String[] getSzoftvers(String Szoftver){
+        return getSzoftvers();
     }   
+
+    @Override
+    public String kiir() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Robot gyariRobot() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getwork() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getsleep() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
 
 
-class IrodaiRobot extends Textúra implements ISzoftver, IWorker{
+class IrodaiRobot extends Textúra implements ISzoftvers, IWorker{
     Robot irodaiRobot;
     String Szoftver;
+
+    public IrodaiRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
     @Override
     public String getID() {
        return "RendörTesztRobot ID" + irodaiRobot.getID();
@@ -269,16 +355,42 @@ class IrodaiRobot extends Textúra implements ISzoftver, IWorker{
     public String getElv() {
         return "RendörTesztRobot Elve: " + irodaiRobot.getElv();
     }
-    public void work();
-    public void sleep();
-    public String[] getSzoftvers(){
-        return Szoftver;
+    public String[] getSzoftvers(String Szoftver){
+        return getSzoftvers();
     } 
+
+    @Override
+    public String kiir() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Robot gyariRobot() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getwork() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getsleep() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
 
-class SzereloRobot extends Textúra implements ISzoftver, IWorker{
+class SzereloRobot extends Textúra implements ISzoftvers, IWorker{
     Robot szereloRobot;
     String Szoftver;
+
+    public SzereloRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
     @Override
     public String getID() {
        return "RendörTesztRobot ID" + szereloRobot.getID();
@@ -303,159 +415,183 @@ class SzereloRobot extends Textúra implements ISzoftver, IWorker{
     public String getElv() {
         return "RendörTesztRobot Elve: " + szereloRobot.getElv();
     }
-    public void work();
-    public void sleep();
     public String[] getSzoftvers(){
-	return Szoftver;
+	return getSzoftvers();
     } 
+
+    @Override
+    public String kiir() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Robot gyariRobot() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getwork() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void getsleep() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
 
-abstract class Ötvözet extends Textúra implements ISzoftver, IWorker{
+  abstract class Ötvözet extends Textúra implements ISzoftvers, IWorker{
 	protected Robot robot;
-	String Szoftver;
-	public Ötvözet(String ID, int memoria, String nem, int teljesitmeny, String elv) {
-        super(ID, memoria, nem, teljesitmeny, elv);
-        
-	public String kiir()
-	{ 
-		return ötvözetKiir()+ robot.kiir();
-	}
-	public String getID(){
-		return ötvözetGetID() + robot.getID();
-	}
-	public int getMemoria(){
-		return ötvözetGetMemoria() + robot.getMemoria();
-	}
-	public String getNem(){
-		return ötvözetGetNem() + robot.getNem();
-	}
-	public int getTeljesitmeny(){
-		return ötvözetGetTeljesitmeny() + robot.getTeljesitmeny();
-	}
-	public String getElv(){
-		return ötvözetGetElv() + robot.getElv();
-	}
 	
-	public abstract String ötvözetKiir();
-	public abstract String ötvözetGetID();
-	public abstract int ötvözetGetMemoria();
-	public abstract String ötvözetGetNem();
-	public abstract int ötvözetGetTeljesitmeny();
-	public abstract String ötvözetGetElv();
-	public void work();
-	public void sleep();
-   	public String[] getSzoftvers(){
-	 	return Szoftver;
+	public Ötvözet(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);	
     }     
+
+    
 }
 
-abstract class GyáriTesztRobot extends Ötvözet implements ISzoftver, IWorker{
-	Robot GyáriTesztRobot(Robot robot){this.robot = robot;}
+abstract class GyáriTesztRobot extends Ötvözet implements ISzoftvers, IWorker{
+
+    public GyáriTesztRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
+	Robot GyáriTesztRobot(Robot robot){this.robot = robot;return null;
+}
 	String Szofver;
-	@Override
+	
 	public String ötvözetKiir(){return "GyáriTesztRobot" + robot.kiir();}
-	@Override
+	
 	public String ötvözetGetID(){return "GyáriTesztRobot ID" + robot.getID();}
-	@Override
+	
 	public int ötvözetGetMemoria(){return  robot.getMemoria()/32768;}
-	@Override
+	
 	public String ötvözetGetNem(){return "GyáriTesztRobot Neme" + robot.getNem();}
-	@Override
+	
 	public int ötvözetGetTeljesitmeny(){return  robot.getTeljesitmeny();}
-	@Override
+	
 	public String ötvözetGetElv(){return "GyáriTesztRobot Elve: " + robot.getElv();}
-	public void work();
-	public void sleep();
-   	 public String[] getSzoftvers(){
-		 return Szoftver;
+   	public String[] getSzoftvers(String szoftver){
+	 	return getSzoftvers();
+    }
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 }
 	
-abstract class RendörTesztRobot extends Ötvözet implements ISzoftver, IWorker{
+abstract class RendörTesztRobot extends Ötvözet implements ISzoftvers, IWorker{
+
+    public RendörTesztRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
     
-	Robot RendörTesztRobot(Robot robot){this.robot = robot;}
+	Robot RendörTesztRobot(Robot robot){this.robot = robot;return null;
+}
 	String Szoftver;
-	@Override
+	
 	public String ötvözetKiir(){return "RendörTesztRobot" + robot.kiir();}
-	@Override
+	
 	public String ötvözetGetID(){return "RendörTesztRobot ID" + robot.getID();}
-	@Override
+	
 	public int ötvözetGetMemoria(){return  + robot.getMemoria()/16348;}
-	@Override
+	
 	public String ötvözetGetNem(){return "RendörTesztRobot Neme" + robot.getNem();}
-	@Override
+	
 	public int ötvözetGetTeljesitmeny(){return  robot.getTeljesitmeny();}
-	@Override
+	
 	public String ötvözetGetElv(){return "RendörTesztRobot Elve: " + robot.getElv();}
-	public void work();
-	public void sleep();
-    	 public String[] getSzoftvers(){
-	 	return Szoftver;
+    	public String[] getSzoftvers(String szoftver){
+	 	return getSzoftvers();
     }	
-}
-	
-abstract class HáziTesztRobot extends Ötvözet implements ISzoftver, IWorker{
-	Robot HáziTesztRobot(Robot robot){this.robot = robot;}
-	String Szoftver;
-	@Override
-	public String ötvözetKiir(){return "HáziTesztRobot" + robot.kiir();}
-	@Override
-	public String ötvözetGetID(){return "HáziTesztRobot ID" + robot.getID();}
-	@Override
-	public int ötvözetGetMemoria(){return   robot.getMemoria()/4096;}
-	@Override
-	public String ötvözetGetNem(){return "HáziTesztRobot Neme" + robot.getNem();}
-	@Override
-	public int ötvözetGetTeljesitmeny(){return  robot.getTeljesitmeny();}
-	@Override
-	public String ötvözetGetElv(){return "HáziTesztRobot Elve: " + robot.getElv();}
-	public void work();
-	public void sleep();
-    	 public String[] getSzoftvers(){
-	 	return Szoftver;
-    }	
-}
-	
-abstract class IrodaiTesztRobot extends Ötvözet implements ISzoftver, IWorker{
-	Robot IrodaiTesztRobot(Robot robot){this.robot =robot;}
-	String Szoftver;
-	@Override
-	public String ötvözetKiir(){return "IrodaiTesztRobot" + robot.kiir();}
-	@Override
-	public String ötvözetGetID(){return "IrodaiTesztRobot ID" + robot.getID();}
-	@Override
-	public int ötvözetGetMemoria(){return  robot.getMemoria()/65536;}
-	@Override
-	public String ötvözetGetNem(){return "IrodaiTesztRobot Neme" + robot.getNem();}
-	@Override
-	public int ötvözetGetTeljesitmeny(){return   robot.getTeljesitmeny();}
-	@Override
-	public String ötvözetGetElv(){return "IrodaiTesztRobot Elve: " + robot.getElv();}
-	public void work();
-	public void sleep();
-   	 public String[] getSzoftvers(){
-		 return Szoftver;
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 }
 	
-abstract class SzereloTesztRobot extends Ötvözet implements ISzoftver, IWorker{
-	Robot  SzereloTesztRobot(Robot robot){this.robot = robot;}
+abstract class HáziTesztRobot extends Ötvözet implements ISzoftvers, IWorker{
+
+    public HáziTesztRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
+	Robot HáziTesztRobot(Robot robot){this.robot = robot;return null;
+}
 	String Szoftver;
-	@Override
-	public String ötvözetKiir(){return "SzereloTesztRobot" + robot.kiir();}
-	@Override
-	public String ötvözetGetID(){return "SzereloTesztRobot ID" + robot.getID();}
-	@Override
-	public int ötvözetGetMemoria(){return   robot.getMemoria()/16384;}
-	@Override
-	public String ötvözetGetNem(){return "SzereloTesztRobot Neme" + robot.getNem();}
-	@Override
-	public int ötvözetGetTeljesitmeny(){return   robot.getTeljesitmeny();}
-	@Override
-	public String ötvözetGetElv(){return "SzereloTesztRobot Elve: " + robot.getElv();}
-	public void work();
-	public void sleep();
-    	 public String[] getSzoftvers(){
-		 return Szoftver;
+	
+	public String ötvözetKiir(){return "HáziTesztRobot" + robot.kiir();}
+	
+	public String ötvözetGetID(){return "HáziTesztRobot ID" + robot.getID();}
+	
+	public int ötvözetGetMemoria(){return   robot.getMemoria()/4096;}
+	
+	public String ötvözetGetNem(){return "HáziTesztRobot Neme" + robot.getNem();}
+	
+	public int ötvözetGetTeljesitmeny(){return  robot.getTeljesitmeny();}
+	
+	public String ötvözetGetElv(){return "HáziTesztRobot Elve: " + robot.getElv();}
+    	public String[] getSzoftvers(String szoftver){
+	 	return getSzoftvers();
     }	
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+}
+	
+abstract class IrodaiTesztRobot extends Ötvözet implements ISzoftvers, IWorker{
+
+    public IrodaiTesztRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
+	Robot IrodaiTesztRobot(Robot robot){this.robot =robot;return null;
+}
+	String Szoftver;
+	
+	public String ötvözetKiir(){return "IrodaiTesztRobot" + robot.kiir();}
+	
+	public String ötvözetGetID(){return "IrodaiTesztRobot ID" + robot.getID();}
+	
+	public int ötvözetGetMemoria(){return  robot.getMemoria()/65536;}
+	
+	public String ötvözetGetNem(){return "IrodaiTesztRobot Neme" + robot.getNem();}
+	
+	public int ötvözetGetTeljesitmeny(){return   robot.getTeljesitmeny();}
+	
+	public String ötvözetGetElv(){return "IrodaiTesztRobot Elve: " + robot.getElv();}
+   		public String[] getSzoftvers(String szoftver){
+	 	return getSzoftvers();
+    }
+
+    private String[] getSzoftvers() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+}
+	
+abstract class SzereloTesztRobot extends Ötvözet implements ISzoftvers, IWorker{
+
+    public SzereloTesztRobot(String ID, int memoria, String nem, int teljesitmeny, String elv) {
+        super(ID, memoria, nem, teljesitmeny, elv);
+    }
+	Robot  SzereloTesztRobot(Robot robot){this.robot = robot;return null;
+}
+	String Szoftver;
+	
+	public String ötvözetKiir(){return "SzereloTesztRobot" + robot.kiir();}
+	
+	public String ötvözetGetID(){return "SzereloTesztRobot ID" + robot.getID();}
+	
+	public int ötvözetGetMemoria(){return   robot.getMemoria()/16384;}
+	
+	public String ötvözetGetNem(){return "SzereloTesztRobot Neme" + robot.getNem();}
+	
+	public int ötvözetGetTeljesitmeny(){return   robot.getTeljesitmeny();}
+	
+	public String ötvözetGetElv(){return "SzereloTesztRobot Elve: " + robot.getElv();}
+    	public String[] getSzoftvers(String szoftver){
+	 	return getSzoftvers();
+    }	
+
+    private String[] getSzoftvers() {
+        return getSzoftvers(); 
+    }
 }
